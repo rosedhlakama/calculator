8b72a366e58c6bafc64d73ed630c160eb01b03cd
rosedhlakama <rosejembe@live.com>
2019-07-23 14:24:11 +1200

    set up js file

---
+++ calc.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+// define variants numericals, operands, clear, equals, entry, answer
+var numericals = document.querySelectorAll('[data-numerical]');
+var operands = document.querySelectorAll('[data-operand]');
+var clearAll = document.querySelector('[data-allClear]');
+var Delete = document.querySelector('[data-delete]');
+var equals = document.querySelector('[data-equals]');
+var decimal = document.querySelector('[data-equal]');
+var entry = document.querySelector('[data-entry]');
+var answer = document.querySelector('[data-answer]')
+
+
+}
